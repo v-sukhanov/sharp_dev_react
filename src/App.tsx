fb372db3830e927e05c6b033ca3fccd51e4e0e2b
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-01-02 09:55:59 +0300

    fetch data from fake api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Product } from './components/Product';
-import { products } from './data/products';
+import axios from 'axios';
+import { IProduct } from './models';
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([])
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+    setProducts(response.data);
+  }
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-        <Product product={products[0]}/>
+        {products.map(product => <Product product={product} key={product.id}/>)}
     </div>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,13 +1,31 @@
 import { IProduct } from '../models';
+import { useState } from 'react';
 
 interface IProductProps {
 	product: IProduct;
 }
 
-export function Product(props: IProductProps) {
+export function Product({ product }: IProductProps) {
+	const [details, setDetails] = useState(false)
+	const btnClassName = details ? 'bg-blue-400' : 'bg-yellow-400';
+	const btnClasses = ['py-2 px-4 border', btnClassName]
 	return (
 		<div className="border py-2 px-4 rounded flex flex-col  items-center mb-2">
-			{props.product.title }
+			<img src={product.image} className="w-1/6" alt=""/>
+			<span>{product.title}</span>
+			<span className="font-bold">{product.price}</span>
+			<button onClick={() => setDetails(prev => !prev)} className={btnClasses.join(' ')}>
+				{details ? 'Hide Details' : 'Show Details'}
+			</button>
+			{details && <div>
+				<span>
+					{product.description}
+				</span>
+				<br/>
+				<span>
+					Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span>
+				</span>
+			</div>}
 		</div>
 	)
 }
