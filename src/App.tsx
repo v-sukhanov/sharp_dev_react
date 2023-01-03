96cad380339c7099e0cc036b991c295888452ae8
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-01-03 21:48:24 +0300

    created modal components

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import { Product } from './components/Product';
 import { useProducts } from './hooks/products';
 import { Error } from './components/error';
 import { Loader } from './components/loader';
+import { Modal } from './components/Modal';
+import { CreateProduct } from './components/CreateProduct';
 
 function App() {
-  const {products, loading, error} = useProducts()
+	const { products, loading, error } = useProducts()
 
-  return (
-    <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <Loader/>}
-      {error && <Error error={error} />}
-        {products.map(product => <Product product={product} key={product.id}/>)}
-    </div>
-  );
+	return (
+		<div className="container mx-auto max-w-2xl pt-5">
+			{loading && <Loader/>}
+			{error && <Error error={error}/>}
+			{products.map(product => <Product product={product} key={product.id}/>)}
+			<Modal>
+				<CreateProduct></CreateProduct>
+			</Modal>
+		</div>
+)
+	;
 }
 
 export default App;
