7371c89f0c477bd321b362f645b367b844e687b1
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-01-04 19:36:51 +0300

    Created user page where we can invite users

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,6 +1,15 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 import axios from 'axios';
-import { json } from 'react-router-dom';
+
+export const Success = ({ count, changeSuccess }: { count: number, changeSuccess: () => void; }) => {
+	return (
+		<div>
+			<h3 className="text-2xl">Успешно!</h3>
+			<p>Всем {count} пользователям отправлено приглашение.</p>
+			<button onClick={changeSuccess} className="px-5 py-2 rounded bg-blue-500 w-full mt-5 text-white">Назад</button>
+		</div>
+	);
+};
 
 export interface IUser {
 	avatar: string;
@@ -11,27 +20,24 @@ export interface IUser {
 }
 
 export interface IUsersListProps {
-	users: IUser[]
+	users: IUser[];
+	addInvites: (id: number) => void;
+	changeSuccess: () => void;
+	invites: number[];
 }
 
-export const UsersList = ({users}: IUsersListProps) => {
+export const UsersList = ({ users, addInvites, invites, changeSuccess }: IUsersListProps) => {
 	const [search, setSearch] = useState('');
-	const [invites, setInvites] = useState<number[]>([]);
-	const handleAddOrDeleteInvites = (id: number) => {
-		setInvites(prev => {
-			if (prev.includes(id)) {
-				return prev.filter(x => x !== id);
-			}
-			return [...prev, id];
-		})
-	}
+
 	return (
 		<div>
 			<div className="search mb-5">
 				<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-					<path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+					<path
+						d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
 				</svg>
-				<input onChange={(event: ChangeEvent<HTMLInputElement>) => setSearch(event.target.value)} type="text" placeholder="Найти пользователя..." />
+				<input onChange={(event: ChangeEvent<HTMLInputElement>) => setSearch(event.target.value)} type="text"
+				       placeholder="Найти пользователя..."/>
 			</div>
 			<li>
 				{
@@ -44,24 +50,25 @@ export const UsersList = ({users}: IUsersListProps) => {
 					}).map(user => {
 						return (
 							<div key={user.id} className="flex justify-between items-center gap-5 mb-5">
-									<div className="flex items-center gap-5">
-										<img className="h-[50px] rounded-full" src={user.avatar} alt="User" />
-										<div>
-											<h3>{user.first_name + ' ' + user.last_name}</h3>
-											<p className="text-gray-400">
-												{user.email}
-											</p>
-										</div>
+								<div className="flex items-center gap-5">
+									<img className="h-[50px] rounded-full" src={user.avatar} alt="User"/>
+									<div>
+										<h3>{user.first_name + ' ' + user.last_name}</h3>
+										<p className="text-gray-400">
+											{user.email}
+										</p>
 									</div>
-									<span onClick={() => handleAddOrDeleteInvites(user.id)} className="transition hover:text-black text-gray-400 text-4xl cursor-pointer">
+								</div>
+								<span onClick={() => addInvites(user.id)}
+								      className="transition hover:text-black text-gray-400 text-4xl cursor-pointer">
 										{invites.find(x => x === user.id) ? '-' : '+'}
 									</span>
-								</div>
+							</div>
 						)
 					})
 				}
 			</li>
-			<button className="px-5 py-2 rounded bg-blue-500 w-full mt-5 text-white">Отправить приглашение</button>
+			<button onClick={invites.length ? changeSuccess : () => null} className="px-5 py-2 rounded bg-blue-500 w-full mt-5 text-white">Отправить приглашение</button>
 
 		</div>
 
@@ -71,6 +78,16 @@ export const UsersList = ({users}: IUsersListProps) => {
 
 export const Users = () => {
 	const [users, setUsers] = useState<IUser[]>([]);
+	const [success, setSuccess] = useState(false);
+	const [invites, setInvites] = useState<number[]>([]);
+	const handleAddOrDeleteInvites = (id: number) => {
+		setInvites(prev => {
+			if (prev.includes(id)) {
+				return prev.filter(x => x !== id);
+			}
+			return [...prev, id];
+		})
+	}
 	useEffect(() => {
 		axios.get('https://reqres.in/api/users')
 			.then(x => setUsers(x.data.data))
@@ -80,7 +97,10 @@ export const Users = () => {
 	return (
 		<div className="h-screen bg-purple-200 flex items-center justify-center">
 			<div className="p-10 bg-white rounded-2xl ">
-				<UsersList users={users}></UsersList>
+				{!success ?
+					<UsersList changeSuccess={() => setSuccess(true)} invites={invites} addInvites={handleAddOrDeleteInvites} users={users}></UsersList>
+					: <Success changeSuccess={() => setSuccess(false)} count={invites.length}/>
+				}
 			</div>
 		</div>
 	)
